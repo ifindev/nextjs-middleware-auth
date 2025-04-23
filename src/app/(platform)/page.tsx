564bbb51f05fc3cde6bab3f620507421e637e15c
ifindev <arifin.muhammad2610@Gmail.com>
2025-04-23 15:20:22 +0700

    refactor(auth): update logout and user retrieval methods
    - Replaced jwtLogoutAction with logoutAction in the Home component for consistency with the new authentication structure.
    - Updated user retrieval in the ProfilePage component to use authRepository.getUser() instead of getUserFromCookies, aligning with the new repository pattern for authentication management.

--- src/app/(platform)/page.tsx
+++ src/app/(platform)/page.tsx
@@ -1,9 +1,9 @@
-import { jwtLogoutAction } from '@/actions/jwt-auth.action';
+import { logoutAction } from '@/actions/auth.action';
 
 export default async function Home() {
     return (
         <div className="flex flex-col gap-2">
-            <form action={jwtLogoutAction} className="flex gap-2">
+            <form action={logoutAction} className="flex gap-2">
                 <button type="submit" className="bg-red-500 text-white p-2 rounded-md w-full">
                     Logout
                 </button>
--- src/app/(platform)/profile/page.tsx
+++ src/app/(platform)/profile/page.tsx
@@ -1,7 +1,7 @@
-import { getUserFromCookies } from '@/libs/jwt-auth.lib';
+import authRepository from '@/libs/auth/repository/auth.repository.impl';
 
 export default async function ProfilePage() {
-    const user = await getUserFromCookies();
+    const user = await authRepository.getUser();
     return (
         <pre className="border border-gray-200 rounded-md p-2 w-full text-xs text-wrap">
             {JSON.stringify(user, null, 2)}
