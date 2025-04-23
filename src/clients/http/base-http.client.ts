9dc7bac29a05bd511a1c30b7ef24a8a97ed076e4
ifindev <arifin.muhammad2610@Gmail.com>
2025-04-23 15:17:08 +0700

    feat(http): implement BaseHttpClient and IHttpClient interface
    - Added BaseHttpClient class for handling HTTP requests with built-in support for authorization and error handling.
    - Introduced IHttpClient interface to define standard methods for HTTP operations (GET, POST, PATCH, DELETE, PUT).
    - Enhanced request handling with dynamic headers and cookie management.

---
+++ src/clients/http/base-http.client.ts
@@ -0,0 +1,126 @@
+import { cookies } from 'next/headers';
+import { IHttpClient } from './http.client.interface';
+import { COOKIE_NAME } from '@/constants/cookie.constant';
+
+export interface BaseHttpClientConfig {
+    baseUrl?: string;
+    headers?: Record<string, string>;
+}
+
+export class BaseHttpClient implements IHttpClient {
+    baseUrl: string;
+    headers?: Record<string, string>;
+
+    constructor(config: BaseHttpClientConfig) {
+        this.baseUrl = config.baseUrl ?? '';
+        this.headers = config.headers ?? {};
+    }
+
+    getDefaultHeaders(): Record<string, string> {
+        return {
+            'Content-Type': 'application/json',
+        };
+    }
+
+    async getBearerToken(): Promise<string> {
+        const cookieStore = await cookies();
+        const accessToken = cookieStore.get(COOKIE_NAME.ACCESS_TOKEN)?.value ?? '';
+        return `Bearer ${accessToken}`;
+    }
+
+    handleError(error: unknown): never {
+        throw error;
+    }
+
+    private async handleResponse<T>(response: Response): Promise<T> {
+        return response.json();
+    }
+
+    updateConfig(config: BaseHttpClientConfig): void {
+        if (config.baseUrl) this.baseUrl = config.baseUrl;
+        if (config.headers) this.headers = { ...this.headers, ...config.headers };
+    }
+
+    private async buildHeaders(
+        headers: Record<string, string> = {},
+    ): Promise<Record<string, string>> {
+        const cookieStore = await cookies();
+        const bearerToken = await this.getBearerToken();
+        return {
+            ...this.getDefaultHeaders(),
+            ...this.headers,
+            ...headers,
+            Cookie: cookieStore.toString(),
+            Authorization: bearerToken,
+        };
+    }
+
+    private async makeRequest<T>(url: string, config: RequestInit): Promise<T> {
+        try {
+            const headers = await this.buildHeaders(config.headers as Record<string, string>);
+            const requestOptions = {
+                ...config,
+                headers,
+            };
+
+            if (config.method === 'GET' || config.method === 'HEAD') {
+                delete requestOptions.body;
+            }
+
+            const response = await fetch(`${this.baseUrl}${url}`, requestOptions);
+
+            if (!response.ok)
+                return this.handleError({
+                    ...(await response.json()),
+                    status: await response.status,
+                });
+            return await this.handleResponse<T>(response);
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    async get<T>(url: string, config?: RequestInit): Promise<T> {
+        return this.makeRequest<T>(url, {
+            ...config,
+            method: 'GET',
+        });
+    }
+
+    async post<T>(url: string, data?: object, config?: RequestInit): Promise<T> {
+        return this.makeRequest<T>(url, {
+            ...config,
+            body: JSON.stringify(data),
+            method: 'POST',
+        });
+    }
+
+    async patch<T>(url: string, data?: object, config?: RequestInit): Promise<T> {
+        return this.makeRequest<T>(url, {
+            ...config,
+            body: JSON.stringify(data),
+            method: 'PATCH',
+        });
+    }
+
+    async delete<T>(url: string, config?: RequestInit): Promise<T> {
+        return this.makeRequest<T>(url, {
+            ...config,
+            method: 'DELETE',
+        });
+    }
+
+    async put<T>(url: string, data?: object, config?: RequestInit): Promise<T> {
+        return this.makeRequest<T>(url, {
+            ...config,
+            body: JSON.stringify(data),
+            method: 'PUT',
+        });
+    }
+}
+
+const httpClient = new BaseHttpClient({
+    baseUrl: process.env.BACKEND_API_URL ?? '',
+});
+
+export default httpClient;
