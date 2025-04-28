f3f93b6c93e3dcd605ed39fc46bbd9578b9ed28c
ifindev <arifin.muhammad2610@Gmail.com>
2025-04-28 15:20:46 +0700

    refactor(profile): convert ProfilePage to use client-side rendering with hooks
    - Changed ProfilePage from an async function to a functional component using React hooks.
    - Implemented useEffect to fetch user data on component mount and store it in local state.
    - Updated the rendering logic to display user information in a preformatted text block.

--- src/app/(platform)/profile/page.tsx
+++ src/app/(platform)/profile/page.tsx
@@ -1,7 +1,16 @@
-import authRepository from '@/libs/auth/repository/auth.repository.impl';
+'use client';
 
-export default async function ProfilePage() {
-    const user = await authRepository.getUser();
+import { User } from '@/libs/auth/models/user.model';
+import authRepository from '@/libs/auth/repository/auth.repository.impl';
+import { useEffect, useState } from 'react';
+
+export default function ProfilePage() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        authRepository.getUser().then(setUser);
+    }, []);
+
     return (
         <pre className="border border-gray-200 rounded-md p-2 w-full text-xs text-wrap">
             {JSON.stringify(user, null, 2)}
